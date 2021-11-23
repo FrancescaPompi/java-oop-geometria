177b9550668a5783492a9ca82c9a66be66754b7a
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-23 15:23:23 +0100

    attributi, costruttori e metodi

--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -2,6 +2,25 @@ package org.generation.italy.geometria;
 
 public class Rettangolo {
 	
+	// attributi
+	int base;
+	int altezza;
 	
+	// costruttore
+	Rettangolo(int base, int altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	// metodi
+	int calcolaArea() {
+		int calcoloArea = this.base * this.altezza;
+		return calcoloArea;
+	}
+	
+	int calcolaPerimetro() {
+		int calcolaPerimetro = 2*this.base + 2*this.altezza;
+		return calcolaPerimetro;
+	}
 	
 }
