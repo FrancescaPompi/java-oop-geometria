cb215d725b0da9212ffad6c8ab340d41d7da2714
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-23 15:41:59 +0100

    fatto main

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,9 +1,22 @@
 package org.generation.italy.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Base: ");
+		int base = scan.nextInt();
+		System.out.print("Altezza: ");
+		int altezza = scan.nextInt();
+		
+		Rettangolo r1 = new Rettangolo(base, altezza);
+		System.out.println("Perimetro: " + r1.calcolaPerimetro());
+		System.out.println("Area: " + r1.calcolaArea());
+		
+		scan.close();
 
 	}
 
